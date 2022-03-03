6618860e69a5df74487fbdf4c9355470e6eca849
Dave Mohamad <david.mohamad@cambridgeconsultants.com>
2022-03-03 09:48:10 +0000

    Restructured code for multi axis sensing
    Defined a force plate that takes three sensors for xyz measurements.
    Data is transmitted to the host via a modified data structure.

--- LoadCellReader.ino
+++ LoadCellReader.ino
@@ -4,48 +4,53 @@
 // Any digital outputs can be used here. Chip is read by bit-banging these
 // pins. It does not use any interrupts or standard comms protocols eg spi or
 // i2c. So far tested 2&3, 4&5 and 6&7.
-const int LOADCELL_DOUT_PIN = 6;
-const int LOADCELL_SCK_PIN = 7;
+const int SENSOR_X_DOUT_PIN = 2;
+const int SENSOR_X_SCK_PIN = 3;
+const int SENSOR_Y_DOUT_PIN = 4;
+const int SENSOR_Y_SCK_PIN = 5;
+const int SENSOR_Z_DOUT_PIN = 6;
+const int SENSOR_Z_SCK_PIN = 7;
 // const int BAUD_RATE = 9600;
 const int BAUD_RATE = 19200;
 
 // note that changing gain/input takes time.
 enum Gain {
-    CH_A_HIGH_GAIN = 128,
+    CH_A_HIGH_GAIN = 128, // default
     CH_B_LOW_GAIN = 32,
     CH_A_MID_GAIN = 64,
 };
 
-
-class LoadCellImpl : public LoadCell {
+class HX711ForceSensor : public ForceSensor::Sensor {
 public:
-    LoadCellImpl(HX711& device) : device_(device) {};
-    int32_t read_channel_a(void) {return device_.read();};
-    int32_t read_channel_b(void) {return 0L;};
+    HX711ForceSensor(HX711& device) : device_(device) {};
+    float newtons(void) {return 0.0;};
+    void update(void) {raw_data_ = device_.read();};
+    int32_t raw_data(void) {return raw_data_;};
 
 private:
     HX711& device_;
+    int32_t raw_data_ = 0L;
 };
 
-HX711 device;
-LoadCellImpl loadcell(device);
-ForceSensor sensor(loadcell);
-
-void write_to_serial(const ForceMeasurement_t& data)
-{
-    // data and checksum written at once...
-    Serial.flush();
-    Serial.write((const uint8_t*)&data, sizeof(ForceMeasurement_t));
-}
+HX711 device_x, device_y, device_z;
+HX711ForceSensor sensor_x(device_x);
+HX711ForceSensor sensor_y(device_y);
+HX711ForceSensor sensor_z(device_z);
+ForceSensor::Plate force_plate(sensor_x, sensor_y, sensor_z);
 
 void setup(void)
 {
     Serial.begin(BAUD_RATE);
-    device.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-    device.set_gain(CH_A_HIGH_GAIN);
+    device_x.begin(SENSOR_X_DOUT_PIN, SENSOR_X_SCK_PIN);
+    device_x.set_gain(CH_A_HIGH_GAIN);
+    device_y.begin(SENSOR_Y_DOUT_PIN, SENSOR_Y_SCK_PIN);
+    device_y.set_gain(CH_A_HIGH_GAIN);
+    device_z.begin(SENSOR_Z_DOUT_PIN, SENSOR_Z_SCK_PIN);
+    device_z.set_gain(CH_A_HIGH_GAIN);
 }
 
 void loop(void)
 {
-    write_to_serial(sensor.read());
+    force_plate.update();
+    force_plate.transmit();
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,45 +1,62 @@
 #include <cassert>
+#include <cmath>
+#include <vector>
 // code under test...
 #include "../forcesensor.h"
 // test dependencies...
+#include "Arduino.h"
 #include "mocks.h"
 
 void test_checksum_non_overflowing(void)
 {
-    FakeLoadCell cell;
-    set_fake_millis(1UL);
-    cell.set_channel_a(1L);
-    cell.set_channel_b(1L);
-    ForceSensor sensor(cell);
-    ForceMeasurement_t reading = sensor.read();
-    assert(3U == reading.checksum);
+    FakeSensor sensor_x, sensor_y, sensor_z;
+    millis(1UL);
+    sensor_x.raw_data(1L);
+    sensor_y.raw_data(1L);
+    sensor_z.raw_data(1L);
+    ForceSensor::Plate force_plate(sensor_x, sensor_y, sensor_z);
+    force_plate.update();
+    ForceSensor::Measurement_t reading = force_plate.read();
+    assert(4U == reading.checksum);
 }
 
 void test_checksum_overflowing(void)
 {
-    FakeLoadCell cell;
-    set_fake_millis(1UL);
-    cell.set_channel_a(255L);
-    cell.set_channel_b(1L);
-    ForceSensor sensor(cell);
-    ForceMeasurement_t reading = sensor.read();
-    assert(1U == reading.checksum);
+    FakeSensor sensor_x, sensor_y, sensor_z;
+    millis(1UL);
+    sensor_x.raw_data(255L);
+    ForceSensor::Plate force_plate(sensor_x, sensor_y, sensor_z);
+    force_plate.update();
+    ForceSensor::Measurement_t reading = force_plate.read();
+    assert(0U == reading.checksum);
 }
 
-// void test_correct_data_written_to_serial(void)
-// {
-//     FakeLoadCell cell;
-//     set_fake_millis(20UL);
-//     cell.set_channel_a(33L);
-//     cell.set_channel_b(1L);
-//     ForceSensor sensor(cell);
-//     ForceMeasurement_t reading = sensor.read();
-//     Serial.write(uint_reading)
-// }
+void test_correct_data_written_to_serial(void)
+{
+    serial_handle.reset();
+    FakeSensor sensor_x, sensor_y, sensor_z;
+    millis(1UL);
+    sensor_x.raw_data(1L);
+    sensor_x.newtons(2.0);
+    sensor_y.raw_data(1L);
+    sensor_z.raw_data(1L);
+    ForceSensor::Plate force_plate(sensor_x, sensor_y, sensor_z);
+    force_plate.update();
+    force_plate.transmit();
+    // compare against data written to serial.
+    const ForceSensor::Measurement_t* actual =
+        (const ForceSensor::Measurement_t*)serial_handle.buffer();
+    assert(1UL == actual->timestamp);
+    assert(1L == actual->data_x);
+    assert(1L == actual->data_y);
+    assert(1L == actual->data_z);
+    assert(fabs(2.0 - actual->force_x) < 0.01);
+}
 
 int main(int argc, char const *argv[])
 {
     test_checksum_non_overflowing();
     test_checksum_overflowing();
+    test_correct_data_written_to_serial();
     return 0;
 }
